956e9bc3dcccc95af97eb46c8a686d1f2eb53dd4
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-09-22 16:48:12 -0500

    In order to do the native implementation test using the plugin and to be able to use it in dart, we created a method that will generate a random color in the different modules (swift, kotli and dart)

--- lib/flutter_example_two_plugin.dart
+++ lib/flutter_example_two_plugin.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/services.dart';
 
 import 'flutter_example_two_plugin_platform_interface.dart';
 
 class FlutterExampleTwoPlugin {
+  static const MethodChannel _methodChannel =
+      MethodChannel('flutter_example_two_plugin');
+
   Future<String?> getPlatformVersion() {
     return FlutterExampleTwoPluginPlatform.instance.getPlatformVersion();
+  }
+
+  static Future<Color> generateColor() async {
+    final randomColor = await _methodChannel.invokeMethod("generateColor");
+    return Color.fromRGBO(randomColor[0], randomColor[1], randomColor[2], 1.0);
   }
 }
